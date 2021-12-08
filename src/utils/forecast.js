4917e9d3be9803d08d0f0d6701f76c0ebc86e2f5
Devansh Gupta <devansh.gupta@ad.fc.net>
2021-12-08 13:58:15 +0530

    Weather Data Added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,10 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location');
     } else {
+      console.log(body.current);
       callback(
         '',
-        `${body.current.weather_descriptions[0]}.It is current ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+        `${body.current.weather_descriptions[0]}.It is current ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.Humidity is ${body.current.humidity}% `
       );
     }
   });
